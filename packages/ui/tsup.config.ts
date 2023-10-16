99d77076dd4afd848a3553021ffc628d14add771
sgrasland <grasland.sylvain@hotmail.fr>
2023-10-16 18:12:30 +0200

    clean = false in ui package

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -7,7 +7,7 @@ export default defineConfig((options: Options) => ({
   banner: {
     js: "'use client'",
   },
-  clean: true,
+  clean: false,
   format: ["cjs", "esm"],
   external: ["react"],
   dts: true,
